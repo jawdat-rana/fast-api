addc7dd6337549cfe42736fbcccfcc75f9998e17
jawdat-rana <jawdat_rana@hotmail.com>
2023-09-05 19:10:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from fastapi import FastAPI
 from routers import nbim
-from mangum import Mangum
 from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
 app.include_router(nbim.router)
 
-handler = Mangum(app)
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
     html = '''
-        API Working - <a href="http://127.0.0.1:8000/docs">Click Here</a> to check documentation
+        API Working - <a href="https://fast-api-norges-bank-investment.onrender.com/docs">Click Here</a> to check documentation
     '''
     return html
