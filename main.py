a457a5cc2f9868a42e13e5d198ce37e154ed1ddd
jawdat-rana <jawdat_rana@hotmail.com>
2024-01-11 14:42:13 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ app.include_router(nbim.router)
 @app.get("/", response_class=HTMLResponse)
 async def root():
     html = '''
-        API Working - <a href="https://fast-api-norges-bank-investment.onrender.com/docs">Click Here</a> to check documentation
+        API Working - <a href="https://fast-api-norges-bank-investment.onrender.com/docs">Click Here</a> to check documentation. 
     '''
     return html
